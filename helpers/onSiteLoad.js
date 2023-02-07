240c819a2e83f528d77ead030ca1655de200203a
Perry Angelora <peamaillist@gmail.com>
2023-02-07 13:40:49 -0500

    Initial commit

---
+++ helpers/onSiteLoad.js
@@ -0,0 +1,22 @@
+export default function onSiteLoad() {
+  console.clear();
+  console.log(`%c
+  Hey, thanks for checking me out!
+  Why don't you say hello.
+  https://github.com/MisterPea
+  https://www.linkedin.com/in/perry-angelora/
+     ___   
+    /\\  \\
+   /::\\  \\ 
+  /:/\\:\\__\\
+ /:/ /:/  /
+/:/_/:/  / 
+\\:\\/:/  /  
+ \\::/__/   
+  \\:\\  \\   
+   \\:\\__\\  
+    \\/__/   
+`, `font-family: monospace`);
+}
+
+
