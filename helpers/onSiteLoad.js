ea264d4447213a0bd5477c0b1c1400446b3fc8ee
Perry Angelora <peamaillist@gmail.com>
2023-02-08 22:15:00 -0500

    Minor tweaks

--- helpers/onSiteLoad.js
+++ helpers/onSiteLoad.js
@@ -3,20 +3,21 @@ export default function onSiteLoad() {
   console.log(`%c
   Hey, thanks for checking me out!
   Why don't you say hello?
-  https://github.com/MisterPea
-  https://www.linkedin.com/in/perry-angelora/
-     ___   
-    /\\  \\
-   /::\\  \\ 
-  /:/\\:\\__\\
- /:/ /:/  /
-/:/_/:/  / 
-\\:\\/:/  /  
- \\::/__/   
-  \\:\\  \\   
-   \\:\\__\\  
-    \\/__/   
-`, `font-family: monospace`);
+
+  • https://github.com/MisterPea
+  • https://www.linkedin.com/in/perry-angelora/
+  %c     ___   
+      /\\  \\
+     /::\\  \\ 
+    /:/\\:\\__\\
+   /:/ /:/  /
+  /:/_/:/  / 
+  \\:\\/:/  /  
+   \\::/__/   
+    \\:\\  \\   
+     \\:\\__\\  
+      \\/__/   
+`,   'color:#d1c5ba; font-size: 12px;', 'color: #aca095; font-size: 12px;');
 }
 
 
