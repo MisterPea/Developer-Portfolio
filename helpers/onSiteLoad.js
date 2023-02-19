f08203aa79dc6ae32f8078f90729c454dc2b0441
Perry Angelora <peamaillist@gmail.com>
2023-02-18 21:58:31 -0500

    Update console.log

--- helpers/onSiteLoad.js
+++ helpers/onSiteLoad.js
@@ -1,24 +1,26 @@
 export default function onSiteLoad() {
-  console.clear();
-  console.log(`%c
-  Hey, thanks for checking me out!
-  Why don't you say hello?
+console.clear();
+console.log(`%c
+Hey, thanks for checking me out!
+Why don't you say hello?
 
-  • https://github.com/MisterPea
-  • https://www.linkedin.com/in/perry-angelora/
-%c        ___   
-       /\\  \\  
-      /::\\  \\ 
-     /:/\\:\\__\\
-    /:/ /:/  /
-   /:/_/:/  / 
-   \\:\\/:/  /  
-    \\::/__/   
-     \\:\\  \\   
-      \\:\\__\\  
-       \\/__/  
-       
-`,   'color:#d1c5ba; font-size: 12px;', 'color: #aca095; font-size: 12px; font-family: monospace');
+• https://github.com/MisterPea
+• https://www.linkedin.com/in/perry-angelora/
+%c
+..............................
+..............___............. 
+............./\\  \\............
+............/::\\  \\........... 
+.........../:/\\:\\__\\..........
+........../:/ /:/  /..........
+........./:/_/:/  /........... 
+.........\\:\\/:/  /............
+..........\\::/__/............. 
+...........\\:\\  \\............. 
+............\\:\\__\\............
+.............\\/__/............
+..............................
+`, 'color:#d1c5ba; font-size: 12px;', 'color: #aca095; font-size: 12px; font-family: monospace, monospace-ui');
 }
 
 
